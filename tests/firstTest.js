8e47edf5c8c5a622c1bf2beb0415b74cb4ee3500
Samuel Hoe Yan Looi <samhylooi@gmail.com>
2019-11-28 15:54:53 +1300

    minimal template nightcode code working

--- tests/firstTest.js
+++ tests/firstTest.js
@@ -1,8 +1,8 @@
 module.exports = {
-    'My first test case'(browser) {
-        browser
-            .url('https://news.ycombinator.com/')
-            .waitForElementVisible('.hnname')
-            .assert.containsText(".hnname", "Hacker News");
-    }
+  'My first test case' (browser) {
+    browser
+      .url('https://news.ycombinator.com/')
+      .waitForElementVisible('.hnname')
+      .assert.containsText('.hnname', 'Hacker News')
+  }
 }
